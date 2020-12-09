da6e83a65a4013b6f2663effb69552bf48ee086e
Tjibbe van der Laan <tjibbevanderlaan@gmail.com>
2020-12-09 22:53:04 +0100

    Restructured app.py

--- app.py
+++ app.py
@@ -14,44 +14,12 @@ def index():
     """Video streaming home page."""
     return render_template('index.html')
 
-
 def gen(camera):
     """Video streaming generator function."""
     while True:
         frame = camera.get_frame()
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-
-@app.route('/boardfeed_details')
-def boardfeed_details():
-    try:
-        camera = Camera()
-    except Camera.CalibrateException as err:
-        # Could not calibrate board, so no content to display
-        error_message = {'status': 'failed', 'message': str(err)}
-        return jsonify(error_message), 200
-    else:
-        return jsonify({'status': 'success'}), 200
-
-# @app.route('/start_feed')
-# def start_feed():
-#     print('ping to start_feed')
-#     try:
-#         camera = Camera()
-#     except Camera.CalibrateException as err:
-#         response = {'status': 'failed', 'message': str(err)}
-
-#         # if there are details which can be shared in the frontend,
-#         # add that to response
-#         if isinstance(err.details, BoardMarkerException):
-#             response['name'] = err.details.__class__.__name__
-#             response['details'] = err.details.props
-
-#         # return response
-#         return jsonify(response), 200
-
-#     return jsonify({'status': 'success'}), 200
-
 
 @app.route('/boardfeed')
 def boardfeed():
@@ -65,6 +33,16 @@ def boardfeed():
         """Video streaming route. Put this in the src attribute of an img tag."""
         return Response(gen(Camera()),mimetype=mimetype)
 
+@app.route('/boardfeed_details')
+def boardfeed_details():
+    try:
+        camera = Camera()
+    except Camera.CalibrateException as err:
+        # Could not calibrate board, so no content to display
+        return jsonify({'status': 'failed', 'message': str(err)}), 200
+    else:
+        return jsonify({'status': 'success'}), 200
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, threaded=True)
+    app.run(host='0.0.0.0', port=80, threaded=True)
