cda4be871c3c8c87a5c7c4d933dd723bfa00f5e6
Tjibbe van der Laan <tjibbevanderlaan@gmail.com>
2020-12-09 22:46:09 +0100

    Fixed launch issues, rewrite to tondo

--- static/app.js
+++ static/app.js
@@ -4,14 +4,14 @@
  */
 
 /**
- * WhiteBoardCameraApp is the controller which 
- * fethes the camera status and video stream from the 
- * backend and controls the frontend viewer to show 
- * the information 
+ * TondoAppController is the controller which 
+ * fetches the camera feed. If this fails, the controller
+ * will fetch details to inform the user why calibration
+ * of the camera failed. 
  */
-var WhiteboardCameraApp = function() {
-    this.statusbar = new StatusBar(this); // statusbar viewer
-    this.board = new Board(this); // board viewer
+var TondoAppController = function() {
+    this.statusbar = new TondoStatusBar(this); // statusbar viewer
+    this.board = new TondoBoard(this); // board viewer
     this.api = {
         "feed": "/boardfeed",
         "details": "/boardfeed_details"
@@ -19,28 +19,24 @@ var WhiteboardCameraApp = function() {
 }
 
 /**
- * init will start the WhiteBoardCameraApp
+ * init will start the TondoAppController
  */
-WhiteboardCameraApp.prototype.launch = function() {
+TondoAppController.prototype.launch = function() {
     // initialize board, set viewer
     this.statusbar.init();
     this.board.hide();
-
-    // create refs
-    api = this.api;
-    launchFailed = this.launchFailed;
-    launchSucceeded = this.launchSucceeded;
+    tac = this;
 
     // fetch status from backend
-    fetch(api.feed).then(function(response) {
+    fetch(tac.api.feed).then(function(response) {
         // all good? show board
-        if(response.status === 200) return launchSucceeded();
+        if(response.status === 200) return tac.launchSucceeded();
 
         // not good? check details
-        fetch(api.details).then(function(response) {
+        fetch(tac.api.details).then(function(response) {
             if(response.status !== 200) {
-                launchFailed();
-                console.log('TondoNotReachable: Backend returns not 200 for detailed info. Status Code: ' + response.status)
+                tac.launchFailed('TondoNotReachable: Backend returns not 200 ' +
+                            'for detailed info. Status Code: ' + response.status);
                 return;
             }
             
@@ -50,31 +46,31 @@ WhiteboardCameraApp.prototype.launch = function() {
    
                 failure_type = data.name
                 missing_markers = data.details && data.details.missing;
-                launchFailed(failure_type, missing_markers);
+                tac.launchFailed('', failure_type, missing_markers);
             });
-        });
-    });
+        }).catch(function(err){ tac.launchFailed(err) });
+    }).catch(function(err){ tac.launchFailed(err) });
 }
 
 /**
  * launchSucceeded is the controller function which arranges that the viewer
  * will show the board
  */
-WhiteboardCameraApp.prototype.launchSucceeded = function() {
-    this.board.setSource(this.feedurl);
+TondoAppController.prototype.launchSucceeded = function() {
+    this.board.setSource(this.api.feed);
     this.statusbar.success();
     this.board.show();
 }
 
 /**
- * launchFailed is the controlelr function which makes sure the error
+ * launchFailed is the controller function which makes sure the error
  * message is correctly organized by te viewer
- * @param  {String} failure_type    Optional: describes type of faulter
+ * @param  {String} failure_type    Optional: describes type of fault
  * @param  {String} missing_markers Optional: describes markers missing
  */
-WhiteboardCameraApp.prototype.launchFailed = function(failure_type, missing_markers) {
+TondoAppController.prototype.launchFailed = function(err, failure_type, missing_markers) {
+    if(err) console.log(err);
     this.statusbar.failed(failure_type, missing_markers);
-    if(consolemsg) console.log(consolemsg);
 }
 
 /**
@@ -82,15 +78,15 @@ WhiteboardCameraApp.prototype.launchFailed = function(failure_type, missing_mark
  * @param  {String} name The name of the whiteboard-element to be found
  * @return {DOMelement}      DOMElement 
  */
-WhiteboardCameraApp.prototype._getElement = function(name) {
-    return document.getElementById('wca-' + name);
+TondoAppController.prototype._getElement = function(name) {
+    return document.getElementById('tondo-' + name);
 };
 
 /**
- * StatusBar is the viewer of status in the whiteboard app
- * @param {Object} mainApp The parent object where StatusBar is part of
+ * TondoStatusBar is the viewer of status in the whiteboard app
+ * @param {Object} mainApp The parent object where TondoStatusBar is part of
  */
-var StatusBar = function(mainApp) {
+var TondoStatusBar = function(mainApp) {
 
     this.getElement = mainApp._getElement;
     this.ele = {
@@ -115,7 +111,7 @@ var StatusBar = function(mainApp) {
 /**
  * init will show the initialzing status message of the app
  */
-StatusBar.prototype.init = function() {
+TondoStatusBar.prototype.init = function() {
     this._hideEle(this.ele.nojs);
     this._setStatus('init');
     this._showEle(this.ele.statusbar);
@@ -128,7 +124,7 @@ StatusBar.prototype.init = function() {
  * delay will show the status message that initializing
  * takes longer then expected
  */
-StatusBar.prototype.delay = function() {
+TondoStatusBar.prototype.delay = function() {
     this._clearInitTimeout("delay");
     this._showEle(this.ele.statusbar);
     this.ele.init_text.innerText = "This takes longer then expected...";
@@ -139,7 +135,7 @@ StatusBar.prototype.delay = function() {
  * success will remove the statusbar since loading the 
  * content seems to be succesfull
  */
-StatusBar.prototype.success = function() {
+TondoStatusBar.prototype.success = function() {
     this._clearInitTimeout();
     this._hideEle(this.ele.statusbar);
 }
@@ -150,7 +146,7 @@ StatusBar.prototype.success = function() {
  * @param  {String} failure_type   Optional. 'NoMarkersFound', 'MissingMarkers'
  * @param  {Array} missingmarkers  Optional. Specifies which marker ids are missing
  */
-StatusBar.prototype.failed = function(failure_type, missingmarkers) {
+TondoStatusBar.prototype.failed = function(failure_type, missingmarkers) {
     this._clearInitTimeout();
     this._showEle(this.ele.statusbar);
     switch (failure_type) {
@@ -176,7 +172,7 @@ StatusBar.prototype.failed = function(failure_type, missingmarkers) {
  * _showEle will display an DOM element 
  * @param  {DOMElement} element DOM element to display
  */
-StatusBar.prototype._showEle = function(element) {
+TondoStatusBar.prototype._showEle = function(element) {
     element.classList.remove('hide');
 }
 
@@ -184,14 +180,14 @@ StatusBar.prototype._showEle = function(element) {
  * _hideEle will not display an DOM element
  * @param  {DOMElement} element DOM element to display
  */
-StatusBar.prototype._hideEle = function(element) {
+TondoStatusBar.prototype._hideEle = function(element) {
     element.classList.add('hide');
 }
 
 /**
  * _setStatus will show a particular status
  */
-StatusBar.prototype._setStatus = function(status) {
+TondoStatusBar.prototype._setStatus = function(status) {
     for (item in this.ele.status) {
         ele = this.ele.status[item]
         if (item !== status) this._hideEle(ele);
@@ -204,7 +200,7 @@ StatusBar.prototype._setStatus = function(status) {
  * to inform the user that it took longer then expected, or that
  * something has failed
  */
-StatusBar.prototype._setInitTimeout = function() {
+TondoStatusBar.prototype._setInitTimeout = function() {
     this.timer = {
         'delay': window.setTimeout(function(){ this.delay()}.bind(this), 10000),
         'failed': window.setTimeout(function(){ 
@@ -217,7 +213,7 @@ StatusBar.prototype._setInitTimeout = function() {
 /**
  * _clearInitTimeout will clear the timers set by _setInitTimoeut
  */
-StatusBar.prototype._clearInitTimeout = function(timerName) {
+TondoStatusBar.prototype._clearInitTimeout = function(timerName) {
     // if timer is specified, only clear this timer
     if(timerName) return window.clearTimeout(this.timer[timerName]);
 
@@ -228,12 +224,11 @@ StatusBar.prototype._clearInitTimeout = function(timerName) {
     
 }
 
-
 /**
- * Board is the viewer of the whiteboard video stream
- * @param {Object} mainApp The parent object where Board is part of
+ * TondoBoard is the viewer of the whiteboard video stream
+ * @param {Object} mainApp The parent object where TondoBoard is part of
  */
-var Board = function(mainApp) {
+var TondoBoard = function(mainApp) {
     this.element = mainApp._getElement('board');
     this.holder = mainApp._getElement('board-holder');
 }
@@ -242,24 +237,24 @@ var Board = function(mainApp) {
  * setSource sets the source of the video feed
  * @param {String} sourceUrl URL of video feed of whiteboard
  */
-Board.prototype.setSource = function(sourceUrl) {
+TondoBoard.prototype.setSource = function(sourceUrl) {
     this.holder.src = sourceUrl;
 }
 
 /**
- * show will show the Board 
+ * show will show the TondoBoard 
  */
-Board.prototype.show = function() {
+TondoBoard.prototype.show = function() {
     this.element.classList.remove('hide');
-    this.holder.classList.add('wca-fadein');
-    this.holder.classList.remove('wca-fadeout');
+    this.holder.classList.add('tondo-fadein');
+    this.holder.classList.remove('tondo-fadeout');
 }
 
 /**
- * hide will hide the Board
+ * hide will hide the TondoBoard
  */
-Board.prototype.hide = function() {
+TondoBoard.prototype.hide = function() {
     this.element.classList.add('hide');
-    this.holder.classList.add('wca-fadeout');
-    this.holder.classList.remove('wca-fadein');
+    this.holder.classList.add('tondo-fadeout');
+    this.holder.classList.remove('tondo-fadein');
 }
